02c891bb9e6a3d492cc73c740fb07c6c67dff826
JuanzelS <zel.staton@gmail.com>
2025-05-07 22:50:31 -0700

    commit

--- books_app/extensions.py
+++ books_app/extensions.py
@@ -13,7 +13,15 @@ db = SQLAlchemy(app)
 ###########################
 # Authentication
 ###########################
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-# TODO: Add authentication setup code here!
+from .models import User
 
-bcrypt = None # remove me! (needed so that server will run)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
+
