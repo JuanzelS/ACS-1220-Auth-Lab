474e345e828de345443f0bcd1b12fe36159938cb
JuanzelS <zel.staton@gmail.com>
2025-05-08 06:29:29 -0700

    commit

--- books_app/auth/forms.py
+++ books_app/auth/forms.py
@@ -1,60 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField, TextAreaField
-from wtforms_sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
-from books_app.models import Audience, Book, Author, Genre, User
-from books_app.extensions import app, db, bcrypt
-
-class BookForm(FlaskForm):
-    """Form to create a book."""
-    title = StringField('Book Title',
-        validators=[DataRequired(), Length(min=3, max=80)])
-    publish_date = DateField('Date Published')
-    author = QuerySelectField('Author',
-        query_factory=lambda: Author.query, allow_blank=False)
-    audience = SelectField('Audience', choices=Audience.choices())
-    genres = QuerySelectMultipleField('Genres',
-        query_factory=lambda: Genre.query)
-    submit = SubmitField('Submit')
-
-class AuthorForm(FlaskForm):
-    """Form to create an author."""
-    name = StringField('Author Name', 
-        validators=[DataRequired(), Length(min=3, max=80)])
-    biography = TextAreaField('Biography', 
-        validators=[Length(max=200)])
-    submit = SubmitField('Submit')
-
-class GenreForm(FlaskForm):
-    """Form to create a genre."""
-    name = StringField('Genre Name', 
-        validators=[DataRequired(), Length(min=3, max=80)])
-    submit = SubmitField('Submit')
+from books_app.models import User
+from books_app.extensions import bcrypt
 
 class SignUpForm(FlaskForm):
-    """Form for user signup."""
     username = StringField('User Name',
         validators=[DataRequired(), Length(min=3, max=50)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
-
+    
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
-    """Form for user login."""
     username = StringField('User Name',
         validators=[DataRequired(), Length(min=3, max=50)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
-
+    
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if not user:
             raise ValidationError('No user with that username. Please try again.')
-
+    
     def validate_password(self, password):
         user = User.query.filter_by(username=self.username.data).first()
         if user and not bcrypt.check_password_hash(
